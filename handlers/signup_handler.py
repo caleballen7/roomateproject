b27da71d800c0ba62d81647e5b5c5b44113aa499
laurenfraser2 <laurenfraser222@gmail.com>
2017-07-13 14:48:17 -0400

    basic sign up

--- handlers/signup_handler.py
+++ handlers/signup_handler.py
@@ -3,16 +3,15 @@ import jinja_env
 import logging
 import webapp2
 
-from models import book
 
-class BookHandler(webapp2.RequestHandler):
+
+class SignUpHandler(webapp2.RequestHandler):
     def get(self):
     	logging.info("SecondHandler")
-    	books = book.Book.query().fetch()
     	# do stuff with books...
         html_params = {
             "title": "Second Title",
             "content": "Some stuff about books."
         }
-        template = jinja_env.env.get_template('templates/tmpl.html')
+        template = jinja_env.env.get_template('templates/signup.html')
         self.response.out.write(template.render(html_params))
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import webapp2
 
 from handlers import jinja_env
 from handlers import main_handler
-from handlers import book_handler
+from handlers import signup_handler
 
 jinja_env.init(os.path.dirname(__file__))
 
 app = webapp2.WSGIApplication([
     ('/', main_handler.MainHandler),
-    ('/books', book_handler.BookHandler),
+    ('/signup', signup_handler.SignUpHandler),
 ], debug=True)
