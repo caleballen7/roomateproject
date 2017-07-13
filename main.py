1977c564f254b6fc15d3db54b13bb8b8539ea8bf
Josephcrudup7 <josephcrudup7@gmail.com>
2017-07-13 15:05:19 -0400

    add roomate handler

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import webapp2
 from handlers import jinja_env
 from handlers import main_handler
 from handlers import signup_handler
+from handlers import find_handler
 
 jinja_env.init(os.path.dirname(__file__))
+
 
 app = webapp2.WSGIApplication([
     ('/', main_handler.MainHandler),
     ('/signup', signup_handler.SignUpHandler),
+    ('/find', find_handler.FindHandler),
 ], debug=True)
