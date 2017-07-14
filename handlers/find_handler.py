b8669c171771f93113421db480d688b1fbb7767a
laurenfraser2 <laurenfraser222@gmail.com>
2017-07-14 15:01:45 -0400

    changes

--- handlers/find_handler.py
+++ handlers/find_handler.py
@@ -2,16 +2,16 @@
 import jinja_env
 import logging
 import webapp2
-
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
 
 class FindHandler(webapp2.RequestHandler):
     def get(self):
-        logging.info("findHandler")
         # do stuff with books...
         html_params = {
             "title": "Second Title",
             "content": "Some stuff about books."
         }
-        template = jinja_env.env.get_template('templates/findaroomate.html')
+        template = jinja_env.env.get_template('templates/find.html')
         self.response.out.write(template.render(html_params))
--- handlers/signup_handler.py
+++ handlers/signup_handler.py
@@ -2,16 +2,46 @@
 import jinja_env
 import logging
 import webapp2
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+class NewUser(ndb.Model):
+    clenliness = ndb.StringProperty()
+    weekwake = ndb.StringProperty()
+    weekndwake = ndb.StringProperty()
 
 
 
 class SignUpHandler(webapp2.RequestHandler):
     def get(self):
-    	logging.info("SecondHandler")
-    	# do stuff with books...
-        html_params = {
-            "title": "Second Title",
-            "content": "Some stuff about books."
-        }
-        template = jinja_env.env.get_template('templates/homepage.html')
-        self.response.out.write(template.render(html_params))
+        newuser = NewUser.query().fetch()
+
+        new_user = ""
+        for user in newuser:
+            new_user += "<p>" + "You are " + str(user.clenliness) + " clean. You wake up around " + str(user.weekwake) + " during the week, and " + str(user.weekndwake) + " on the weekends. Here are people that you would be good roommates with!"
+
+
+
+        template = jinja_env.env.get_template('templates/signup.html')
+
+        user_params = {
+            "html_info": new_user,
+            }
+        self.response.out.write(template.render(user_params))
+        #connecting the response from multiple choice with a value
+    def post(self):
+        r_clean = self.request.get("form_clean")
+        r_week = self.request.get("form_week")
+        r_weeknd = self.request.get("form_weeknd")
+
+        new_user = NewUser(clenliness = r_clean, weekwake = r_week, weekndwake = r_weeknd)
+        new_user.put()
+        self.redirect("/find")
+        
+
+
+
+
+
+
+        
