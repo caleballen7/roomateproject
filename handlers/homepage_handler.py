2c249094f275154d26c9e618a8914108852ad5ff
caleballen7 <caleb7harper@gmail.com>
2017-07-14 14:10:31 -0400

    makehompage

--- handlers/homepage_handler.py
+++ handlers/homepage_handler.py
@@ -2,14 +2,17 @@
 import jinja_env
 import logging
 import webapp2
+from google.appengine.api import users
+
 
 
 
 class HomepageHandler(webapp2.RequestHandler):
     def get(self):
-    	logging.info("SecondHandler")
-    	
-        html_params = {
+        logging.info("SecondHandler")
+        
+        homepagedic = {
+          "html_login_url": users.create_login_url('/signup'),
         }
         template = jinja_env.env.get_template('templates/homepage.html')
-        self.response.out.write(template.render(html_params))
+        self.response.out.write(template.render(homepagedic))
