f06cd5e81904c95cf0bca3bc95c78f2631d17b29
laurenfraser2 <laurenfraser222@gmail.com>
2017-07-17 14:31:27 -0400

    made changes

--- handlers/find_handler.py
+++ handlers/find_handler.py
@@ -3,15 +3,19 @@ import jinja_env
 import logging
 import webapp2
 from google.appengine.api import users
-from google.appengine.ext import ndb
+from models import newuser
 
 
 class FindHandler(webapp2.RequestHandler):
     def get(self):
-        # do stuff with books...
-        html_params = {
-            "title": "Second Title",
-            "content": "Some stuff about books."
-        }
+        
+        user = newuser.UserModel.query().get()
+        new_user = "<p>" + "You are " + str(user.clenliness) + " clean. You wake up around " + str(user.weekwake) + " during the week, and " + str(user.weekndwake) + " on the weekends. Here are people that you would be good roommates with!"
+
+        user_params = {
+            "html_info": new_user,
+            }
+        
+        #connecting the response from multiple choice with a value
         template = jinja_env.env.get_template('templates/find.html')
-        self.response.out.write(template.render(html_params))
+        self.response.out.write(template.render(user_params))
--- handlers/signup_handler.py
+++ handlers/signup_handler.py
@@ -4,40 +4,30 @@ import logging
 import webapp2
 from google.appengine.api import users
 from google.appengine.ext import ndb
+from models import newuser
 
-class NewUser(ndb.Model):
-    clenliness = ndb.StringProperty()
-    weekwake = ndb.StringProperty()
-    weekndwake = ndb.StringProperty()
 
 
 
 class SignUpHandler(webapp2.RequestHandler):
     def get(self):
-        newuser = NewUser.query().fetch()
-
-        new_user = ""
-        for user in newuser:
-            new_user += "<p>" + "You are " + str(user.clenliness) + " clean. You wake up around " + str(user.weekwake) + " during the week, and " + str(user.weekndwake) + " on the weekends. Here are people that you would be good roommates with!"
-
-
 
         template = jinja_env.env.get_template('templates/signup.html')
 
-        user_params = {
-            "html_info": new_user,
-            }
-        self.response.out.write(template.render(user_params))
+        self.response.out.write(template.render())
+
         #connecting the response from multiple choice with a value
     def post(self):
         r_clean = self.request.get("form_clean")
         r_week = self.request.get("form_week")
         r_weeknd = self.request.get("form_weeknd")
+        r_email = self.request.get("form_email")
 
-        new_user = NewUser(clenliness = r_clean, weekwake = r_week, weekndwake = r_weeknd)
+        new_user = newuser.UserModel(clenliness = r_clean, weekwake = r_week, weekndwake = r_weeknd)
         new_user.put()
         self.redirect("/find")
-        
+
+
 
 
 
