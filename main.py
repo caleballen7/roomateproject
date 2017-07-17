00dea1a2a840f786f1c5e6f832c66e8ef68b85be
Josephcrudup7 <josephcrudup7@gmail.com>
2017-07-17 14:21:59 -0400

    updated handlers

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import webapp2
 from handlers import jinja_env
 from handlers import main_handler
 from handlers import signup_handler
-from handlers import login_handler
 from handlers import homepage_handler
+from handlers import find_handler
 
 jinja_env.init(os.path.dirname(__file__))
 
@@ -13,5 +13,6 @@ jinja_env.init(os.path.dirname(__file__))
 app = webapp2.WSGIApplication([
     ('/homepage', homepage_handler.HomepageHandler),
     ('/signup', signup_handler.SignUpHandler),
-    ('/login', login_handler.LoginHandler),
+    ('/find', find_handler.FindHandler),
+
 ], debug=True)
